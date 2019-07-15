450acfb98d4842953b9024037ea8de3b52df1c9f
Mark S. Everitt <mark.s.everitt@gmail.com>
2019-07-15 12:20:14 +0100

    Update build-html to build from a template.

--- build-html.js
+++ build-html.js
@@ -4,10 +4,29 @@
 const fs = require('fs');
 const path = require('path');
 
-const indexHtmlFile = path.join(__dirname, 'docs', 'index.html');
+const docsdir = fs.readdirSync(path.join(__dirname, 'docs'));
+const jsFilenames = docsdir.filter(n => n.endsWith('.js'));
 
-const text = fs.readFileSync(indexHtmlFile, 'utf-8');
+if (jsFilenames.length !== 1) {
+    throw new Error('Wrong number of JS files in docs directory:', jsFilenames);
+}
+
+const demoCssFilenames = docsdir.filter(n => n.match(/demo.*\.css/));
+
+if (demoCssFilenames.length !== 1) {
+    throw new Error('Wrong number of demo CSS files in docs directory:', demoCssFilenames);
+}
+
+const indexCssFilenames = docsdir.filter(n => n.match(/index.*\.css/));
+
+if (indexCssFilenames.length !== 1) {
+    throw new Error('Wrong number of index CSS files in docs directory:', indexCssFilenames);
+}
+
+const text = fs.readFileSync(path.join(__dirname, 'src', 'demo', 'index.html'), 'utf-8');
 const patched = text
-    .replace(/build\/app\.js\?.*?"/g, `build/app.js?${Date.now()}"`);
+    .replace('{{module}}', `/${jsFilenames[0]}`)
+    .replace('{{index-css}}', `/${indexCssFilenames[0]}`)
+    .replace('{{demo-css}}', `/${demoCssFilenames[0]}`);
 
-fs.writeFileSync(indexHtmlFile, patched, 'utf-8');
+fs.writeFileSync(path.join(__dirname, 'docs', 'index.html'), patched, 'utf-8');

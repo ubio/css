300c43e55c837d4796547c22d9755e29b5a610eb
muzafarova <alisapsee@gmail.com>
2019-08-01 11:54:32 +0100

    2.1.5

--- docs/index-58478e87.js
+++ docs/index-58478e87.js
@@ -7492,7 +7492,7 @@ if (inBrowser) {
 }
 
 var name = "@ubio/css";
-var version = "2.1.4";
+var version = "2.1.5";
 var description = "UBIO CSS framework foundation";
 var main = "index.css";
 var repository = "git@github.com:universalbasket/css";

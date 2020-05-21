cd9b723e40eb7d5a51b700b0968c5ce5ea4277a9
muzafarova <alisapsee@gmail.com>
2020-05-21 16:23:43 +0100

    2.1.10

--- docs/index-c2c0a9bc.js
+++ docs/index-c2c0a9bc.js
@@ -7492,7 +7492,7 @@ if (inBrowser) {
 }
 
 var name = "@ubio/css";
-var version = "2.1.9";
+var version = "2.1.10";
 var description = "UBIO CSS framework foundation";
 var main = "index.css";
 var repository = "git@github.com:ubio/css";

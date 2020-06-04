f34a5fcd5c73044beec48912a86dbffbda63b32d
muzafarova <alisapsee@gmail.com>
2020-06-04 16:34:25 +0100

    3.0.0

--- docs/index-4ef9b501.js
+++ docs/index-4ef9b501.js
@@ -7492,7 +7492,7 @@ if (inBrowser) {
 }
 
 var name = "@ubio/css";
-var version = "2.1.10";
+var version = "3.0.0";
 var description = "UBIO CSS framework foundation";
 var main = "index.css";
 var repository = "git@github.com:ubio/css";
